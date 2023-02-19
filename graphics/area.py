f2b2202dc487475274da6bd6d7553c5272f5dc39
RIJULROJIO <rijulrojio@gmail.com>
2023-02-19 11:04:09 +0530

    c03

--- graphics/area.py
+++ graphics/area.py
@@ -1,7 +1,7 @@
-import graphics.graphics_3d.cuboid as cub
-import graphics.graphics_3d.sphere as sph
+
 import graphics.circle as circ
 import graphics.rectangle as rect
+from graphics.graphics_3d import cuboid,sphere
 
 radius = int(input("Enter the radius of the circle:"))
 
@@ -13,3 +13,11 @@ print("area of the rectangle  is:",rect.rectangle(len,bth))
 
 print("perimeter  of the rectangle  is:",rect.rectperimeter(len,bth))
 
+l1=int(input("Enter the length of the cuboid :"))
+b1=int(input("Enter the breadth of the cuboid :"))
+h1=int(input("Enter the height of the cuboid :"))
+cuboid.areacub(l1,b1,h1)
+cuboid.pericub(l1,b1,h1)
+r1=int(input("Enter the raduis of the sphere:"))
+sphere.areas(r1)
+sphere.peris(r1)
